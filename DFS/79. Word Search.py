f1206c2d43dff2e5004ca59f1bcfe2317c0f1164
siqi <520serki@gmail.com>
2019-09-20 00:31:17 -0400

    dfs backtracking

---
+++ DFS/79. Word Search.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        if board is None:
+            return False
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, word, i, j):
+                    return True
+        return False
+    
+    def dfs(self, board, word, i, j):
+        if len(word) == 0:
+            return True
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[0] :
+            return False
+        temp = board[i][j]
+        board[i][j] = "#"
+        if self.dfs(board, word[1:], i + 1, j) or self.dfs(board, word[1:], i - 1, j) or self.dfs(board, word[1:], i, j + 1) or self.dfs(board, word[1:], i, j - 1):
+            return True
+        else: 
+            board[i][j] = temp
+        return False
+        
+            
+        
