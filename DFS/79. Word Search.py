252a1a3a99358ba4fe931dc59eaf7d20bb073aaa
siqi <520serki@gmail.com>
2019-10-16 23:42:26 -0400

    dfs

--- DFS/79. Word Search.py
+++ DFS/79. Word Search.py
@@ -5,27 +5,28 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        if board is None:
+        if board is None or word is None:
             return False
-
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if self.dfs(board, word, i, j):
                     return True
         return False
-    
-    def dfs(self, board, word, i, j):
+        
+    def dfs(self,board, word, i, j):
         if len(word) == 0:
             return True
-        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[0] :
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[0]:
             return False
         temp = board[i][j]
         board[i][j] = "#"
-        if self.dfs(board, word[1:], i + 1, j) or self.dfs(board, word[1:], i - 1, j) or self.dfs(board, word[1:], i, j + 1) or self.dfs(board, word[1:], i, j - 1):
-            return True
-        else: 
+        if self.dfs(board, word[1:], i + 1, j) or self.dfs(board, word[1:], i - 1, j) or self.dfs(board, word[1:], i, j + 1) or self.dfs(board, word[1:], i, j -1):
+            return True 
+        else:
             board[i][j] = temp
         return False
+        
         
             
         
